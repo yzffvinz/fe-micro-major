82ac1ce7838944e77f59504383a54b0fae78fac7
yzffvinz <1178043794@qq.com>
2020-04-28 00:44:04 +0800

    1.完成了underscore基础结构的封装 2.完成deepCopy，唯一列表

--- 1-js-pro/1.2.8-underscore/myUnderscore.js
+++ 1-js-pro/1.2.8-underscore/myUnderscore.js
@@ -1,29 +1,77 @@
-(function(root) {
-  let _ = function() {
-    if (!(root._)) {
-      return new _();
-    } else {
-      return root._;
+void function (root) {
+  // 保证new 和 直接执行函数返回一致
+  var _ = function(obj) {
+    if (!(this instanceof _)) {
+      return new _(obj);
     }
+    this.wrap = obj;
   };
 
-  _.unique = function(arr) {
-    const rst = []
-    arr.forEach(item => {
-      if (rst.indexOf(item) === -1) rst.push(item)
-    })
-    return rst
+  _.assert = function assert(a, b) {
+    return a === b;
   };
 
-  _.each = function (arr, callback, step) {
-    for (let i = 0; i < arr.length; i += step || 1) {
-      callback(arr[i], i)
+  _.type = function type(obj) {
+    return Object.prototype.toString.call(obj);
+  };
+
+  _.isArray = function isArray(arr) {
+    return _.assert(_.type(arr), _.type([]));
+  };
+
+  _.isObject = function isObject(obj) {
+    return _.assert(_.type(obj), _.type({}));
+  };
+
+  _.isFunction = function isFunction(func) {
+    return _.assert(_.type(func), _.type(function() {}));
+  }
+
+  _.each = function each(arr, callback) {
+    for (let i = 0; i < arr.length; i++) {
+      callback.call(arr, arr[i], i, arr);
     }
-  };
+  }
 
-  _.mixin = function() {
+  _.map = function map(arr, callback) {
+    let rst = [];
+    for (let i = 0; i < arr.length; i++) {
+      rst.push(callback.call(arr, arr[i], i, arr));
+    }
+    return rst;
+  }
 
-  };
+  _.functions = function functions(obj) {
+    let functions = [];
+    for (let k in obj) {
+      if (_.isFunction(obj[k])) functions.push(k);
+    }
+    return functions;
+  }
 
+  _.prototype.chain = function chain() {
+    this._chain = true;
+    return this;
+  }
+
+  _.prototype.result = function result() {
+    return this.wrap;
+  }
+
+  _.mixin = function mixin(obj) {
+    _.each(_.functions(obj), function(k) {
+      _.prototype[k] = function() {
+        const args = arguments;
+        if (this._chain) {
+          this.wrap = obj[k].apply(this.wrap, [this.wrap, ...args]);
+          return this;
+        } else {
+          return obj[k].apply(this.wrap, [this.wrap, ...arguments]);
+        }
+      };
+    });
+  }
+
+  _.mixin(_);
   root._ = _;
-})(this);
+}(this);
--- 1-js-pro/1.2.9-deepCopy/deepcopy.js
+++ 1-js-pro/1.2.9-deepCopy/deepcopy.js
@@ -10,21 +10,58 @@ function isObject(obj) {
   return Object.prototype.toString.call(obj).slice(8, -1).toLowerCase() === 'object';
 }
 
-function deepCopy(source) {
-  let rst = {};
+
+function find(uniqueList, item) {
+  for (let i = 0; i < uniqueList.length; i++) {
+    uniqueList[i].source === item;
+    return uniqueList[i].target;
+  }
+  return null;
+}
+
+function deepCopy(source, uniqueList) {
+  uniqueList = uniqueList || [];
+  let rst;
+
+  const target = find(uniqueList, source);
+  if (target) {
+    return target;
+  }
+
+  const targetWrapper = {
+    source,
+    target: null
+  }
+  uniqueList.push(targetWrapper);
+
   if (isObject(source)) {
+    rst = {};
+    targetWrapper.target = rst;
     Object.keys(source).forEach(k => {
-      rst[k] = deepCopy(source[k]);
+      rst[k] = deepCopy(source[k], uniqueList);
     })
   } else if (isArray(source)) {
+    rst = [];
+    targetWrapper.target = rst;
     for (let i = 0; i < source.length; i++) {
-      rst[i] = deepCopy(source[i]);
+      rst[i] = deepCopy(source[i], uniqueList);
     }
-  } else if (assert(source), 'symbol') {
-    rst = Symbol(source.description);
   } else {
     rst = source;
+    targetWrapper.target = rst;
   }
   return rst;
 }
 
+let a = {
+  a: 'a'
+};
+
+let b = {
+  b: 'b'
+};
+
+a.b = b;
+b.a = a;
+
+deepCopy(a);
