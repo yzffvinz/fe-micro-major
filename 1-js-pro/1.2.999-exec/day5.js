55aeb265d9db12c7b4827712d642fede9a7e561b
yzffvinz <1178043794@qq.com>
2020-04-29 23:31:56 +0800

    solu1:一个错误的提交

---
+++ 1-js-pro/1.2.999-exec/day5.js
@@ -0,0 +1,33 @@
+/**
+ * @param {string} word1
+ * @param {string} word2
+ * @return {number}
+ */
+var minDistance = function(word1, word2) {
+  let rowLength = word1.length + 1;
+  let colLength = word2.length + 1;
+  let dp = [[]];
+  for (let i = 0; i < rowLength; i++) dp[0].push(i);
+  for (let i = 1; i < colLength; i++) {
+    const row = [i];
+    dp.push(row);
+    for (let j = 1; j < rowLength; j++) {
+      row.push(-1);
+    }
+  }
+
+  for (let i = 1; i < colLength; i++) {
+    for (let j = 1; j < rowLength; j++) {
+      let min = Math.min(dp[i - 1][j - 1], dp[i - 1][j], dp[i][j - 1]);
+      if (word1[j - 1] === word2[i - 1]) {
+        dp[i][j] = min;
+      } else {
+        dp[i][j] = min + 1;
+      }
+    }
+  }
+
+  return dp[colLength - 1][rowLength - 1];
+};
+
+console.log(minDistance('cee', 'ec'))
